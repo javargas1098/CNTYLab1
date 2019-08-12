23b24843c464fff0fa9135681561eb4dd50a710b
Javier Vargas <ja.vargas1098@gmail.com>
2019-08-11 19:49:46 -0500

    falta 7 y 8

--- src/Operations/Complex.java
+++ src/Operations/Complex.java
@@ -3,6 +3,7 @@ package Operations;
 public class Complex {
 	double x;
 	double y;
+	double module;
 
 	public double getReal() {
 		return x;
@@ -23,27 +24,56 @@ public class Complex {
 	public Complex(double real, double y) {
 		this.x = real;
 		this.y = y;
+		this.setModule();
 
 	}
+
+	public double setModule() {
+		return this.module = Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
+		
+	}
+
+	public void setModule(double module) {
+		this.module = module;
+	}
+
 	public String toString() {
 		return x + " " + y + "i";
-		
+
 	}
+
 	public Complex sumComplex(Complex sumcomplejo) {
-		return new Complex(this.x + sumcomplejo.x , this.y + sumcomplejo.y);
-		
+		return new Complex(this.x + sumcomplejo.x, this.y + sumcomplejo.y);
+
 	}
+
 	public Complex resComplex(Complex rescomplejo) {
-		return new Complex(this.x - rescomplejo.x , this.y - rescomplejo.y);
-		
+		return new Complex(this.x - rescomplejo.x, this.y - rescomplejo.y);
+
 	}
+
 	public Complex mulComplex(Complex mulcomplejo) {
-		return new Complex((this.x * mulcomplejo.x -  this.y * mulcomplejo.y),(this.x * mulcomplejo.y + this.y * mulcomplejo.x));
+		return new Complex((this.x * mulcomplejo.x - this.y * mulcomplejo.y),
+				(this.x * mulcomplejo.y + this.y * mulcomplejo.x));
+
+	}
+	public Complex divComplex(Complex divComplex) {
+		return divComplex(this, divComplex);
 		
 	}
 
+	public Complex divComplex(Complex divComplex1, Complex divComplex2) {
+		double xx = (divComplex1.x * divComplex2.x + divComplex1.y * divComplex2.y)
+				/ (Math.pow(divComplex2.x, 2) + Math.pow(divComplex2.y, 2));
+		double yy = (divComplex1.y * divComplex2.x - divComplex1.x * divComplex2.y)
+				/ (Math.pow(divComplex2.x, 2) + Math.pow(divComplex2.y, 2));
+		return new Complex(xx, yy);
 
+	}
+	public Complex conComplex(Complex conComplex) {
+		return new Complex(conComplex.x, -conComplex.y);
+		
+	}
+	
 
 }
-
-
--- src/Operations/pruebas.java
+++ src/Operations/pruebas.java
@@ -4,6 +4,8 @@ public class pruebas {
 	public static void main(String[] args) {
 		Complex c1 = new Complex(3,5);
 		Complex c2 = new Complex(2,-4);
+		System.out.println(c1.setModule());
+		System.out.println(c1.divComplex(c2).toString());
 		System.out.println(c1.mulComplex(c2).toString());
 	}
 
