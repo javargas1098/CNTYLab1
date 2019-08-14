10c315cd48a62ee61a7b028f859543f1b257151f
Javier Vargas <ja.vargas1098@gmail.com>
2019-08-14 00:42:24 -0500

    faltan pruebas

--- src/Operations/Complex.java
+++ src/Operations/Complex.java
@@ -5,24 +5,6 @@ public class Complex {
 	double y;
 	double module;
 	double fase;
-	double degrees;
-	public double getDegrees() {
-		return degrees;
-	}
-
-	public void setDegrees(double degrees) {
-		this.degrees = degrees;
-	}
-
-	public double getRadius() {
-		return radius;
-	}
-
-	public void setRadius(double radius) {
-		this.radius = radius;
-	}
-
-	double radius;
 
 	public double getFase() {
 		return fase;
@@ -54,43 +36,45 @@ public class Complex {
 		this.setModule();
 		this.setFase();
 		this.setCartesiantoPolar();
-		this.setPolartoCartesian();
 
 	}
-	private void setPolartoCartesian(double radius, double degrees) {
+
+	public Complex setPolartoCartesian(double radius, double degrees) {
+		degrees = degrees * (Math.PI/180);
+		x=radius*Math.cos(degrees);
+		y =radius*Math.sin(degrees);
+		return new Complex(x, y);
 		// TODO Auto-generated method stub
-		
-		
+
 	}
 
 	public double[] setCartesiantoPolar() {
 		double[] cartesian = new double[2];
-		double radio = setModule();
-		double angulo = Math.toDegrees(setFase());
-		cartesian[0]=radio;
-		cartesian[1]=angulo;
+		double radius = setModule();
+		double degrees = Math.toDegrees(setFase());
+		cartesian[0] = radius;
+		cartesian[1] = degrees;
 		return cartesian;
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/**
-	 * fase  de un copmlejo 
+	 * fase de un copmlejo
 	 */
 	private double setFase() {
 		// TODO Auto-generated method stub
-		return this.fase = Math.atan(y/x);
-		
-		
+		return this.fase = Math.atan(y / x);
+
 	}
 
 	/**
-	 * Modulo  de un copmlejo 
+	 * Modulo de un copmlejo
 	 */
 
 	public double setModule() {
-		return this.module = Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
-		
+		return this.module = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
 	}
 
 	public void setModule(double module) {
@@ -101,6 +85,7 @@ public class Complex {
 		return x + " + " + y + "i";
 
 	}
+
 	/**
 	 * Suma de complejos
 	 */
@@ -109,16 +94,18 @@ public class Complex {
 		return new Complex(this.x + sumcomplejo.x, this.y + sumcomplejo.y);
 
 	}
+
 	/**
-	 * Resta de complejos 
+	 * Resta de complejos
 	 */
 
 	public Complex resComplex(Complex rescomplejo) {
 		return new Complex(this.x - rescomplejo.x, this.y - rescomplejo.y);
 
 	}
+
 	/**
-	 * CoMultiplicaion de complejos 
+	 * CoMultiplicaion de complejos
 	 */
 
 	public Complex mulComplex(Complex mulcomplejo) {
@@ -126,10 +113,12 @@ public class Complex {
 				(this.x * mulcomplejo.y + this.y * mulcomplejo.x));
 
 	}
+
 	public Complex divComplex(Complex divComplex) {
 		return divComplex(this, divComplex);
-		
+
 	}
+
 	/**
 	 * Division de complejos
 	 */
@@ -139,19 +128,17 @@ public class Complex {
 				/ (Math.pow(divComplex2.x, 2) + Math.pow(divComplex2.y, 2));
 		double numerador = (divComplex1.x * divComplex2.x + divComplex1.y * divComplex2.y)
 				/ (Math.pow(divComplex2.x, 2) + Math.pow(divComplex2.y, 2));
-		
+
 		return new Complex(numerador, denominador);
 
 	}
+
 	/**
 	 * Conjugado de un complejo
 	 */
 	public Complex conComplex(Complex conComplex) {
 		return new Complex(conComplex.x, -conComplex.y);
-		
+
 	}
-	
-	
-	
 
 }
--- src/Operations/pruebas.java
+++ src/Operations/pruebas.java
@@ -6,7 +6,9 @@ public class pruebas {
 	public static void main(String[] args) {
 		Complex c1 = new Complex(4,5);
 		Complex c2 = new Complex(2,-4);
-		System.out.println(Arrays.toString(c1.setCartesian()));
+		Complex c3 = new Complex(0, 0) ;
+		System.out.println(c3.setPolartoCartesian(6.4, 51).toString());
+		System.out.println(Arrays.toString(c1.setCartesiantoPolar()));
 		System.out.println(c1.divComplex(c2).toString());
 		System.out.println(c1.mulComplex(c2).toString());
 	}
