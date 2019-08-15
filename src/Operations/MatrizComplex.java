a004faa6f4f872ede76993f1853d2846a6512b11
javier vargas <ja.vargas1098@gmail.com>
2019-08-15 16:07:20 -0500

    matrixcalculator

--- src/Operations/MatrizComplex.java
+++ src/Operations/MatrizComplex.java
@@ -1,7 +1,7 @@
 package Operations;
 
 public class MatrizComplex {
-	int filas,columnas;
+	private int filas,columnas;
 	Complex [][] matriz;
 	
 	public MatrizComplex(int filas,  int columnas) {
@@ -30,39 +30,6 @@ public class MatrizComplex {
 		this.columnas = columnas;
 	}
 
-	public MatrizComplex sumMatriz(MatrizComplex math1 , MatrizComplex math2) {
-		if (math1.getFilas()==math2.getFilas() && math1.getColumnas()==math2.getColumnas()) {
-			MatrizComplex mathfinal = new MatrizComplex(getFilas(), getColumnas());
-			Complex sumaComplex;
-			for (int i = 0; i < math1.filas; i++) {
-				for (int j = 0; j < math1.getColumnas(); j++) {
-					sumaComplex = math1.getPosition(i, j).sumComplex(math2.getPosition(i, j));
-					mathfinal.addComplex(i, j, sumaComplex);
-					
-				}
-			}
-			return mathfinal;
-		}else {
-			System.out.println("Matrices de dimensiones diferente");
-			return null;
-		}
-	}
-	public MatrizComplex resMatriz(MatrizComplex math1 , MatrizComplex math2) {
-		if (math1.getFilas()==math2.getFilas() && math1.getColumnas()==math2.getColumnas()) {
-			MatrizComplex mathfinal = new MatrizComplex(getFilas(), getColumnas());
-			Complex sumaComplex;
-			for (int i = 0; i < math1.filas; i++) {
-				for (int j = 0; j < math1.getColumnas(); j++) {
-					sumaComplex = math1.getPosition(i, j).resComplex(math2.getPosition(i, j));
-					mathfinal.addComplex(i, j, sumaComplex);
-					
-				}
-			}
-			return mathfinal;
-		}else {
-			System.out.println("Matrices de dimensiones diferente");
-			return null;
-		}
-	}
+	
 
 }
