0aef6fb0bc7d70cbe0124d722ee22c19bcc06837
javargas1098 <43441798+javargas1098@users.noreply.github.com>
2019-08-15 13:43:05 -0500

    Update CalculatorTest.java

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -2,7 +2,7 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import operations.*;
+import Operations.*;
 
 public class CalculatorTest {
 	@Test
