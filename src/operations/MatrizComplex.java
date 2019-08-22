1bfe440734d22f567902b9584d0706208104f4c2
unknown <2124259@labinfo.is.escuelaing.edu.co>
2019-08-22 15:14:39 -0500

    faltan 4 puntos

--- src/operations/MatrizComplex.java
+++ src/operations/MatrizComplex.java
@@ -1,35 +1,51 @@
 package Operations;
 
 public class MatrizComplex {
-	private int filas,columnas;
-	Complex [][] matriz;
-	
-	public MatrizComplex(int filas,  int columnas) {
+	private int filas, columnas;
+	Complex[][] matriz;
+
+	public MatrizComplex(int filas, int columnas) {
 		// TODO Auto-generated constructor stub
-		this.filas=filas;
-		this.columnas=columnas;
+		this.filas = filas;
+		this.columnas = columnas;
 		matriz = new Complex[filas][columnas];
 	}
-	public void addComplex (int fila, int columna, Complex number) {
-		matriz[fila][columna]= number; 
+
+	public void addComplex(int fila, int columna, Complex number) {
+		matriz[fila][columna] = number;
 	}
-	public Complex getPosition (int fila, int columna) {
+
+	public Complex getPosition(int fila, int columna) {
 		return matriz[fila][columna];
 	}
-	
+
 	public int getFilas() {
 		return filas;
 	}
+
 	public void setFilas(int filas) {
 		this.filas = filas;
 	}
+
 	public int getColumnas() {
 		return columnas;
 	}
+
 	public void setColumnas(int columnas) {
 		this.columnas = columnas;
 	}
 
-	
+	public String toString() {
+		String matrix = "";
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+				matrix += "  " + this.matriz[i][j];
+
+			}
+			matrix += "\n";
+
+		}
+		return matrix;
+	}
 
 }
--- src/test/pruebas.java
+++ src/test/pruebas.java
@@ -3,16 +3,47 @@ package test;
 import java.util.Arrays;
 
 import Operations.Complex;
+import Operations.MatrizComplex;
+import Operations.MatrixCalculator;
 
 public class pruebas {
 	public static void main(String[] args) {
-		Complex c1 = new Complex(4,5);
-		Complex c2 = new Complex(2,-4);
-		Complex c3 = new Complex(0, 0);
-		Complex c4 = new Complex(4, 3);
+		
+		Complex c1 = new Complex(2,2);
+		Complex c2 = new Complex(2,0);
+		Complex c3 = new Complex(1, -4);
+		Complex c4 = new Complex(0, 0);
+		Complex c5 = new Complex(2, 3);
+		Complex c6 = new Complex(6, 0);
+		
+		Complex c7 = new Complex(3,0);
+		Complex c8 = new Complex(1,4);
+		Complex c9 = new Complex(2, 2);
+		Complex c10 = new Complex(5, 0);
+		Complex c11 = new Complex(0, 0);
+		Complex c12 = new Complex(3, 9);
+		
+		MatrizComplex mat1 = new MatrizComplex(2, 3);
+		MatrizComplex mat2 = new MatrizComplex(3, 2);
+			mat1.addComplex(0, 0, c1);
+			mat1.addComplex(0, 1, c2);
+			mat1.addComplex(0, 2, c3);
+			mat1.addComplex(1, 0, c4);
+			mat1.addComplex(1, 1, c5);
+			mat1.addComplex(1, 2, c6);
+			
+			mat2.addComplex(0, 0, c7);
+			mat2.addComplex(0, 1, c8);
+			mat2.addComplex(1, 0, c9);
+			mat2.addComplex(1, 1, c10);
+			mat2.addComplex(2, 0, c11);
+			mat2.addComplex(2, 1, c12);
+			
+		
+		
 		//System.out.println(c4.setModule());
 		//System.out.println(c3.setPolartoCartesian(6.4, 51).toString());
-		System.out.println(c3.setCartesiantoPolar(c1).toString());
+		System.out.println(MatrixCalculator.MulMatriz(mat1, mat2));
 		//System.out.println(c1.divComplex(c2).toString());
 		//System.out.println(c1.mulComplex(c2).toString());
 	}
