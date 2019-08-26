fe2960100bb617f869b786ec4a9c70ab8231cd39
Javier Vargas <ja.vargas1098@gmail.com>
2019-08-25 19:50:15 -0500

    pruebas lab1

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -12,10 +12,11 @@ public class CalculatorTest {
 		Complex c1 = new Complex(4, 3);
 		Complex c2 = new Complex(3, -1);
 		Complex c3 = c1.sumComplex(c2);
+		Complex answer = new Complex(7, 2);
 		 //System.out.println(c3);
 
 		// Respuesta
-		assertEquals("7.0 + 2.0i",c3.toString());
+		assertEquals(answer,c3);
 	}
 
 	@Test
@@ -25,11 +26,11 @@ public class CalculatorTest {
 		Complex c1 = new Complex(4, 3);
 		Complex c2 = new Complex(3, -1);
 		Complex c3 = c1.resComplex(c2);
+		Complex answer = new Complex(1, 4);
 		// System.out.println(c3);
 
 		// Respuesta
-		assertEquals("1.0 + 4.0i",c3.toString());
-
+		assertEquals(answer,c3);
 	}
 
 	@Test
@@ -39,10 +40,11 @@ public class CalculatorTest {
 		Complex c1 = new Complex(4, 3);
 		Complex c2 = new Complex(3, -1);
 		Complex c3 = c1.mulComplex(c2);
+		Complex answer = new Complex(15, 5);
 		//System.out.println(c3);
 
 		// Respuesta
-		assertEquals("15.0 + 5.0i",c3.toString());
+		assertEquals(answer,c3);
 	}
 
 	@Test
@@ -52,10 +54,11 @@ public class CalculatorTest {
 		Complex c1 = new Complex(4, 3);
 		Complex c2 = new Complex(3, -1);
 		Complex c3 = c1.divComplex(c2);
+		Complex answer = new Complex(0.9, 1.3);
 //		System.out.println(c3);
 
 		// Respuesta 
-		assertEquals("0.9 + 1.3i",c3.toString());
+		assertEquals(answer,c3);
 
 	}
 
@@ -78,10 +81,11 @@ public class CalculatorTest {
 		Complex c1 = new Complex(4, 3);
 		Complex c2 = new Complex(0, 0);
 		Complex c3 = c2.conComplex(c1);
+		Complex answer = new Complex(4, -3);
 		//System.out.println(c3);
 
 		// Respuesta 
-		assertEquals("4.0 + -3.0i",c3.toString());
+		assertEquals(answer,c3);
 	}
 
 
@@ -92,10 +96,11 @@ public class CalculatorTest {
 		// TestCase
 		Complex c1 = new Complex(0,0);
 		Complex c2 = c1.setPolartoCartesian(5.0, 36.86989764584402);
+		Complex answer = new Complex(4, 3);
 		//System.out.println(c2);
 
 		// Respuesta 
-		assertEquals("4.0 + 3.0i",c2.toString());
+		assertEquals(answer,c2);
 
 	}
 
@@ -106,10 +111,11 @@ public class CalculatorTest {
 		Complex c1 = new Complex(4, 3);
 		Complex c2 = new Complex(0, 0);
 		Complex c3 = c2.setCartesiantoPolar(c1);
+		Complex answer = new Complex(5, 36.86989764584402);
 		//System.out.println(c3);
 
 		// Respuesta 
-		assertEquals("5.0 + 36.86989764584402i",c3.toString());
+		assertEquals(answer,c3);
 
 	}
 	@Test
